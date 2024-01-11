39aae3b97297ebe840a3606cd920ce2ab82e5f70
მაცი <93665709+maci03@users.noreply.github.com>
2024-01-11 16:11:44 +0400

    modify all apptsx imports and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import { Route, Routes } from 'react-router-dom'
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import SignUpScreen from './screens/SignUpScreen'
+import OrderScreen from './screens/OrderScreen'
+import CheckOutScreen from './screens/CheckOutScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import Loading from './components/Loading'
+import { auth } from './config/firebase'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [user, loading, error] = useAuthState(auth)
+	console.log(user, loading)
+
+	if (loading) {
+		return <Loading />
+	}
+
+	return (
+		<AppWrapper>
+			{user ? (
+				<Routes>
+					<Route path="/" element={<HomeScreen />} />
+					<Route path="/login" element={<LoginScreen />} />
+					<Route path="/register" element={<SignUpScreen />} />
+					<Route path="/payment" element={<PaymentScreen />} />
+					<Route path="/order" element={<OrderScreen />} />
+					<Route path="/checkout" element={<CheckOutScreen />} />
+				</Routes>
+			) : (
+				<LoginScreen />
+			)}
+		</AppWrapper>
+	)
 }
 
-export default App;
+export default App
+
+const AppWrapper = styled.div`
+	min-height: 100vh;
+	width: 100vw;
+	max-width: 100vw;
+`
