fdca299697f099a368077ec9baba399fc66efd50
მაცი <93665709+maci03@users.noreply.github.com>
2024-01-28 21:56:03 +0400

    starter pack of searchresult comp

---
+++ src/search/SearchResults.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+
+const SearchResults = () => {
+    return(
+        <ResultContainer>
+
+        </ResultContainer>
+    )
+}
+export default SearchResults
