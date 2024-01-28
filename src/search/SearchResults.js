c26602d8077f415e927666c07d4146fbdaff8d92
მაცი <93665709+maci03@users.noreply.github.com>
2024-01-28 22:03:49 +0400

    decleare path to api for search

--- src/search/SearchResults.js
+++ src/search/SearchResults.js
@@ -6,7 +6,13 @@ import { Link } from "react-router-dom";
 const SearchResults = () => {
     return(
         <ResultContainer>
-
+            {results.map((product) => (
+                <ProductCard key={product.id}>
+                    <Link to={`/product/${product.id}`}>
+                        <img src={`/src/data/Data.ts/${product.image}`}alt={product.title} />
+                    </Link>
+                </ProductCard>
+            ))}
         </ResultContainer>
     )
 }
